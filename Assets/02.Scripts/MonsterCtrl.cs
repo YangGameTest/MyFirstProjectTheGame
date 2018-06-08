aa16f9626c9998d6f45850b14b23ec92a45dded2
YangJiaQi <1295585769@qq.com>
2018-06-08 09:41:14 +0800

    更新2018.6.8

--- Assets/02.Scripts/MonsterCtrl.cs
+++ Assets/02.Scripts/MonsterCtrl.cs
@@ -23,6 +23,11 @@ public class MonsterCtrl : MonoBehaviour
     //怪兽是否死亡
     private bool isDie = false;
 
+    //血迹效果预设
+    public GameObject bloodEffect;
+    //血迹贴图效果预设
+    public GameObject bloodDecal;
+
     private void Start()
     {
         //获取怪兽的Transform组件
@@ -118,11 +123,35 @@ public class MonsterCtrl : MonoBehaviour
     {
         if (coll.gameObject.tag=="BULLET")
         {
+            //调用血迹效果函数
+            CreateBloodEffect(coll.transform.position);
+
             //删除子弹对象Bullet
             Destroy(coll.gameObject);
             //触发IsHit Trigger，使怪兽从 Any State 转换为 gothit 状态
             animator.SetTrigger("IsHit");
         }
+    }
+
+    void CreateBloodEffect(Vector3 pos)
+    {
+        //生成血迹效果
+        GameObject bloodl = (GameObject)Instantiate(bloodEffect, pos, Quaternion.identity);
+        Destroy(bloodl, 2.0f);
+
+        //贴图生成位置：计算在地面以上的位置
+        Vector3 dectorPos = monsterTr.position + (Vector3.up * 0.05f);
+        //随机设置贴图旋转值
+        Quaternion decalRot = Quaternion.Euler(90, 0, Random.Range(0, 360));
+
+        //生成贴图预设
+        GameObject blood2 = (GameObject)Instantiate(bloodDecal, dectorPos, decalRot);
+        //调整贴图大小，使其每次生成的尺寸不同
+        float scale = Random.Range(1.5f, 3.5f);
+        blood2.transform.localScale = Vector3.one * scale;
+
+        //5秒后删除血迹效果预设
+        Destroy(blood2, 5.0f);
     }
 
 
